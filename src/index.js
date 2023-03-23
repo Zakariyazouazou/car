e05b0129e4a1e35a78b8742f62c53c2f504c545e
Zakariyazouazou <112694786+Zakariyazouazou@users.noreply.github.com>
2023-03-23 13:42:41 +0100

    upload

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter as  Router } from 'react-router-dom';
+import { HashRouter as  Router ,  } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
